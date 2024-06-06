c28b3f5565b7182cd1631c81cf67f52fea3bee80
Chris Kirkham <me@chriskirkham.com>
2024-06-05 22:46:22 -0600

    feat: added more styles, analytics, speed insights, github bio

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,14 +1,15 @@
 type HeaderProps = {
   name: string;
+  bio: string;
   location: string;
 };
 
-const Header = ({ name, location }: HeaderProps) => {
+const Header = ({ name, bio, location }: HeaderProps) => {
   return (
-    <header className="text-right text-black">
-      <h1 className="font-size-xl">{name}</h1>
-      <h2>Development | Documentary | Digital Filmmaking</h2>
-      <h3>Based in {location}</h3>
+    <header className="text-right pr-20 py-10 text-white bg-gray-500/35 w-lvw">
+      <h1 className="text-8xl">{name}</h1>
+      <h2 className="text-2xl">{bio}</h2>
+      <h3 className="text-lg">Based in {location}</h3>
     </header>
   );
 };
