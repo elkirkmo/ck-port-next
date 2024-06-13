a3dc67bfaefe47f1a830f25e3219db1c675679d5
Chris Kirkham <me@chriskirkham.com>
2024-06-13 14:07:47 -0600

    feat: added content json file

---
+++ app/content.js
@@ -0,0 +1,16 @@
+export const content = {
+  pages: {
+    development: {
+      linkText: 'Some of the web projects I&apos; been working on lately',
+    },
+    documentary: {
+      linkText: 'Examples of my documentary work',
+    },
+    live: {
+      linkText: 'Come see me live! Pub Trivia, Comedy and More',
+    },
+    film: {
+      linkText: 'Examples of my work in film',
+    },
+  },
+};
