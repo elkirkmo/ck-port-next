afb7f9400f53b4d438c26e5a3bfdefbf25f1f272
Chris Kirkham <chris@chriss-mbp.lan>
2025-08-06 12:19:40 -0600

    adding babel and jest

--- app/components/video.tsx
+++ app/components/video.tsx
@@ -1,29 +1,40 @@
-import { Suspense } from 'react';
+"use client";
+
+import React, { useEffect, useState, Suspense } from 'react';
 
 export default function VideoBG() {
+  const [fileName, setFileName] = useState<string | null>(null);
+
+  useEffect(() => {
+    const videoNames =
+      process.env.NEXT_PUBLIC_BG_VIDEO_ARRAY?.split(',').map((v) => v.trim()) ||
+      [];
+    if (videoNames.length > 0) {
+      const idx = Math.floor(Math.random() * videoNames.length);
+      setFileName(videoNames[idx].trim());
+    }
+  }, []);
+
+  if (!fileName) return null;
+
   return (
     <div className="absolute left-0 top-0 w-lvw l-dvh object-cover -z-10">
-      <Suspense fallback={<p>Loading video...</p>}>
-        <Video fileName={randomBackground()} />
+         <Suspense fallback={<div>Loading video...</div>}>
+        <Video fileName={fileName} />
       </Suspense>
     </div>
   );
 }
 
-const backgroundVideoNames = process.env.BG_VIDEO_ARRAY?.split(',') || [];
-const randomBackground = () =>
-  backgroundVideoNames[
-    (Math.random() * backgroundVideoNames.length) | 0
-  ].trim();
-
 interface VideoProps {
   fileName: string;
 }
 
-const Video = async ({ fileName }: VideoProps) => {
-  const buildUrl = () => `${process.env.S3_BUCKET_URL}${fileName}`;
+function Video({ fileName }: VideoProps) {
+  const buildUrl = () =>
+    `${process.env.NEXT_PUBLIC_S3_BUCKET_URL}${fileName}`;
   return (
-    <div className=" absolute left-0 top-0 w-lvw h-dvh object-cover">
+    <div className="absolute left-0 top-0 w-lvw h-dvh object-cover">
       <video
         className="absolute left-0 top-0 w-lvw l-dvh object-cover -z-10"
         playsInline
@@ -36,4 +47,4 @@ const Video = async ({ fileName }: VideoProps) => {
       </video>
     </div>
   );
-};
+}
